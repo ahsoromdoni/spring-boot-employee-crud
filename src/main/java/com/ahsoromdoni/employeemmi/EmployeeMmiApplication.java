07338c7edce1b636342d92313638d6b2befb79dc
ASRomdon <dablegcoblan@gmail.com>
2020-08-22 21:20:16 +0700

    Innit Project Spring Boot

---
+++ src/main/java/com/ahsoromdoni/employeemmi/EmployeeMmiApplication.java
@@ -0,0 +1,13 @@
+package com.ahsoromdoni.employeemmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeMmiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ahsoromdoni/employeemmi/EmployeeMmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahsoromdoni.employeemmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
